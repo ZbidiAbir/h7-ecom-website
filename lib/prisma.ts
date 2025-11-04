0073f8429c7fe259a9077ff80c582c625641a271
Abir Zbidi <abir.zbidi.visioad@gmail.com>
2025-11-04 17:44:23 +0100

    fix error build

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,12 @@
 import { PrismaClient } from "@/app/generated/prisma";
 
 const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
+  return new PrismaClient();
+};
 declare const globalThis: {
   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-export default prisma
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+export default prisma;
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
